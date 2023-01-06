8065b7290c4e4979338f0d62c6f4ac319e2d3a9e
schoolsyaks <92911997+schoolsyaks@users.noreply.github.com>
2023-01-06 21:57:47 +0100

    Update createRGAzure.tf

--- createRGAzure.tf
+++ createRGAzure.tf
@@ -15,9 +15,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "example2" {
+  name     = "example2"
+  location = "North Europe"
 }
